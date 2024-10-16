3f9d17edbf39a4c01fb5f8692bdc67b0919a46ec
10kartik <10kartikkapgate@gmail.com>
2024-10-16 11:11:36 +0530

    added adsense script, add use client, use effect and removed metadata object

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,25 @@
+"use client";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "ATS Score Booster by KK",
-  description: "Developed by KK",
-};
-
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    if (window) {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    }
+  }, []);
   return (
     <>
       <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8729812116098552"
+          crossorigin="anonymous"
+        ></script>
         <title>ATS Score Booster by KK</title>
         <meta
           name="description"
@@ -24,7 +31,19 @@ export default function RootLayout({ children }) {
         />
       </Head>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          {children}
+          <ins
+            className="adsbygoogle"
+            style={{ display: "block" }}
+            data-ad-client="ca-pub-8729812116098552"
+            data-ad-slot="7045878436"
+            data-ad-format="auto"
+          ></ins>
+          <script>
+            {`(adsbygoogle = window.adsbygoogle || []).push({});`}
+          </script>
+        </body>
       </html>
     </>
   );
